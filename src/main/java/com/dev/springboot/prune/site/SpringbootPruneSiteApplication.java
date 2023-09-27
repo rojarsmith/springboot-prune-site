fd99a2553e60d579d64311f7cf592fb9fa23e8c4
Rojar Smith <rojarsmith@gmail.com>
2023-09-27 21:05:33 +0800

    Initial Project

---
+++ src/main/java/com/dev/springboot/prune/site/SpringbootPruneSiteApplication.java
@@ -0,0 +1,13 @@
+package com.dev.springboot.prune.site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootPruneSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootPruneSiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev/springboot/prune/site/SpringbootPruneSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.springboot.prune.site;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootPruneSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
