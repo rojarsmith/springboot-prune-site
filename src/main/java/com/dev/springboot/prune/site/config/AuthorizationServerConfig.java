b30c1aba55fcc5549f046f59bd519cf8fd52924f
Rojar Smith <rojarsmith@gmail.com>
2023-09-28 13:19:44 +0800

    Add Spring Authorization Server with Minimal Configuration

---
+++ src/main/java/com/dev/springboot/prune/site/config/AuthorizationServerConfig.java
@@ -0,0 +1,20 @@
+package com.dev.springboot.prune.site.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.security.oauth2.server.authorization.client.JdbcRegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
+
+@Configuration
+@Import(OAuth2AuthorizationServerConfiguration.class)
+public class AuthorizationServerConfig {
+
+	@Bean
+	RegisteredClientRepository registeredClientRepository(JdbcTemplate jdbcTemplate) {
+		return new JdbcRegisteredClientRepository(jdbcTemplate);
+	}
+
+}
